d9319f6822ecf8686d7ccf6950ee2e931e37d57a
Furkan Çakıcı <furkancakici25@gmail.com>
2026-01-22 15:28:29 +0300

    chore: Delete an unspecified file.

--- apps/admin-panel/vite.config.ts
+++ apps/admin-panel/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: "../../",
   server: {
     port: Number(process.env.ADMIN_PANEL_PORT) || 3001,
   },
